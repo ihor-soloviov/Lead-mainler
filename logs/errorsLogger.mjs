316e34b6714ec835ba4b429734330604a7eeb954
Ihor Soloviov <igor.musson.55@gmail.com>
2023-09-19 16:53:51 +0300

    updated logger with meta data of error

--- logs/errorsLogger.mjs
+++ logs/errorsLogger.mjs
@@ -2,13 +2,24 @@ import winston from "winston";
 
 const { combine, timestamp, printf } = winston.format;
 
-const logFormat = printf(({ timestamp, level, message }) => {
-  return `${timestamp} ${level}: ${message}`;
+const logFormat = printf(({ timestamp, level, message, meta }) => {
+  let logMessage = `${timestamp} ${level}: ${message}`;
+
+  // Додавання додаткового контексту, якщо він є
+  if (meta) {
+    logMessage += `\nAdditional Info: ${JSON.stringify(meta, null, 2)}`;
+  }
+
+  return logMessage;
 });
 
 export const errorLogger = winston.createLogger({
   level: "error",
-  format: combine(timestamp(), logFormat),
+  format: combine(
+    timestamp(),
+    winston.format.metadata(), // Для передачі додаткового контексту
+    logFormat
+  ),
   transports: [
     new winston.transports.File({
       filename: "./logs/error.log",
@@ -16,4 +27,3 @@ export const errorLogger = winston.createLogger({
     }),
   ],
 });
-
