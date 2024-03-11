ec38502e417e735b1b01fc90571f1a02c5bc5b5f
Ihor Soloviov <igor.musson.55@gmail.com>
2024-03-11 17:28:36 +0200

    Fix date formatting in errorsLogger- Date is now displayed in a more readable format

--- logs/errorsLogger.mjs
+++ logs/errorsLogger.mjs
@@ -3,7 +3,17 @@ import winston from "winston";
 const { combine, timestamp, printf } = winston.format;
 
 const logFormat = printf(({ timestamp, level, message, meta }) => {
-  let logMessage = `${timestamp} ${level}: ${message}`;
+  const now = Date.now(); // Отримуємо поточний час у мілісекундах
+  const date = new Date(now); // Перетворюємо мілісекунди в об'єкт Date
+
+  // Форматування дати до зручного вигляду
+  const formattedDate = date.toLocaleDateString('uk-UA', {
+    year: 'numeric', // повний рік
+    month: 'long', // назва місяця
+    day: 'numeric', // день місяця
+    weekday: 'long', // день тижня
+  });
+  let logMessage = `${formattedDate} ${level}: ${message}`;
 
   // Додавання додаткового контексту, якщо він є
   if (meta) {
