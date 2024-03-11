f7f67ae8eae9ce964d5ca3f1214b68b7cf3e7d12
Ihor Soloviov <igor.musson.55@gmail.com>
2024-03-11 17:10:40 +0200

    Update Pipedrive fields and content handling

--- services/pipeDrive.service.mjs
+++ services/pipeDrive.service.mjs
@@ -22,17 +22,33 @@ class PipeDriveService {
     emailService.sendErrorEmail(errorMessage);
   }
 
-  async pipeDriveSender(lead, personId, herkunft) {
+  async pipeDriveSender(lead, personId, heirkunft) {
     const api = new pipedrive.DealsApi(this.createPipedriveClient());
 
-    const { userData, adresse, ...dealDetails } = lead;
+    const {
+      zweck,
+      energieverbrauch,
+      dachForm,
+      dachdatum,
+      verfugbare,
+      adresse,
+      userData,
+      user_id,
+    } = lead;
+
     const title = `ID = ${personId} / ${userData.Vorname} ${userData.Nachname} / ${adresse.Straße} ${adresse.Hausnummer}, ${adresse.PLZ}, ${adresse.Ort}, Deutschland`;
 
     const data = {
       title,
+      user_id: user_id,
       person_id: personId,
-      ...dealDetails,
-      bb79205fc4d894114b9b4d49804f6176d659d002: `${adresse.Straße} ${adresse.Hausnummer}, ${adresse.PLZ}, ${adresse.Ort}, Deutschland`,
+      "57cbeced6be1098f5ca5c884183644b71d29124d": zweck,
+      "16c723ccd64cd776798adb6680e9b8a03adf5515": energieverbrauch,
+      "09b97b8d2bd41b8d73125a7e8bd4eb73c85cdef0": dachForm,
+      "cbed075975beadd5b9c2e6db78e45fbfcee5152c": verfugbare,
+      "1f481fe0db105b6bd860169aa9760cd2c6fcb352": dachdatum,
+      "9ffd55b37d9b787d3481a962700521c8ff99cc2f": heirkunft,
+      "bb79205fc4d894114b9b4d49804f6176d659d002": `${adresse.Straße} ${adresse.Hausnummer}, ${adresse.PLZ}, ${adresse.Ort}, Deutschland`,
     };
 
     try {
@@ -49,7 +65,8 @@ class PipeDriveService {
     try {
       const response = await axios.post(`https://api.pipedrive.com/v1/persons?api_token=${this.apiToken}`, JSONdata, {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
       });
 
@@ -75,8 +92,41 @@ class PipeDriveService {
 
     try {
       const request = await api.getDeal(dealId);
+      let result;
       if (request.success) {
-        // Ваш код обробки даних угоди тут
+        const {
+          id,
+          user_id,
+          person_id,
+          bb79205fc4d894114b9b4d49804f6176d659d002_route,
+          bb79205fc4d894114b9b4d49804f6176d659d002_street_number,
+          bb79205fc4d894114b9b4d49804f6176d659d002_postal_code,
+          bb79205fc4d894114b9b4d49804f6176d659d002_locality,
+        } = request.data;
+
+        const userData = {
+          id,
+          home:
+            bb79205fc4d894114b9b4d49804f6176d659d002_route +
+            " " +
+            bb79205fc4d894114b9b4d49804f6176d659d002_street_number,
+          name: person_id.name,
+          tel: person_id.phone[0].value,
+          email: person_id.email[0].value,
+          code: bb79205fc4d894114b9b4d49804f6176d659d002_postal_code,
+          city: bb79205fc4d894114b9b4d49804f6176d659d002_locality,
+        };
+
+        const sellerData = {
+          name: user_id.name,
+          email: user_id.email,
+          tel: request.data['79feacdf9755a64ad18d02c306062801f28f52b9']
+        };
+
+        result = { userData, sellerData };
+
+        return result
+
       }
     } catch (error) {
       this.logAndSendError(error);
