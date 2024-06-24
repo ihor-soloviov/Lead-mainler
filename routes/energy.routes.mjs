9235587a11826818cbf19db86b6f6780a4d1c72c
Ihor Soloviov <igor.musson.55@gmail.com>
2024-06-24 16:21:53 +0300

    test

--- routes/energy.routes.mjs
+++ routes/energy.routes.mjs
@@ -13,7 +13,7 @@ const energyRouter = Router();
 energyRouter.post('/cv', cvUploader.getMiddleware().single('file'), strapiController.sendCvToStrapi)
 energyRouter.post('/angebot', angebotUploader.getMiddleware().single('file'), strapiController.sendAngebotToStrapi)
 energyRouter.post('/leadgen', textMessagesUploader.none(), strapiController.sendLead)
-energyRouter.put('/leadgen', textMessagesUploader.none(), strapiController.sendAdditionalInfoLead)
+energyRouter.put('/leadgen/:leadId', textMessagesUploader.none(), strapiController.sendAdditionalInfoLead)
 energyRouter.post('/contact-us', textMessagesUploader.none(), strapiController.sendContactUs);
 energyRouter.post('/contact-hero', textMessagesUploader.none(), strapiController.sendContactHero)
 
